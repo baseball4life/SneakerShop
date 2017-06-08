1338a6f014e4f454fb23dc490d749512390c1638
baseball4life <evillani73@gmail.com>
2017-06-07 21:17:26 -0400

    latest changes
    Work done in class

---
+++ homework/main.js
@@ -0,0 +1,12 @@
+console.log("pluged in")
+
+var trigger = document.getElementById('trigger');
+var modal = decument.getElementById('modalWrapper');
+var close = decument.getElementById('modalWrapper');
+
+  trigger.addEventListener('click' function addItem(){
+    console.log('big button clicked');
+
+    model.style.visibility='visible';
+
+  });
